0a03be87946f87d1672304121d029725e5dbc702
rmin2000 <135095340+rmin2000@users.noreply.github.com>
2024-03-13 16:21:49 +0800

    Update resnet.py

--- models/resnet.py
+++ models/resnet.py
@@ -85,57 +85,13 @@ class ResNet(nn.Module):
         out = self.layer4(out)
         out = F.avg_pool2d(out, 4)
         out = out.view(out.size(0), -1)
-        out4 = out
         out = self.linear(out)
         
         return out
-        # return out,out4
-
-
-
-class ResNet_mod(nn.Module):
-    def __init__(self, block, num_blocks, num_classes=10):
-        super(ResNet_mod, self).__init__()
-        self.in_planes = 64
-
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(64)
-        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
-        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
-        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
-        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
-        self.linear = nn.Linear(25088 * block.expansion, num_classes)
-
-    def _make_layer(self, block, planes, num_blocks, stride):
-        strides = [stride] + [1] * (num_blocks - 1)
-        layers = []
-        for stride in strides:
-            layers.append(block(self.in_planes, planes, stride))
-            self.in_planes = planes * block.expansion
-        return nn.Sequential(*layers)
-
-    def forward(self, x):
-        out = F.relu(self.bn1(self.conv1(x)))
-        out = self.layer1(out)
-        out1 = out
-        out = self.layer2(out)
-        out2 = out
-        out = self.layer3(out)
-        out3 = out
-        out = self.layer4(out)
-        out = F.avg_pool2d(out, 4)
-        out = out.view(out.size(0), -1)
-        out4 = out
-        out = self.linear(out)
-        # return out, (out1, out2, out3, out4)
-        # return out, out4
-        return out
+       
 
 def ResNet18(num_classes=10):
     return ResNet(BasicBlock, [2, 2, 2, 2], num_classes)
-
-def ResNet18_mod(num_classes=10):
-    return ResNet_mod(BasicBlock, [2, 2, 2, 2], num_classes)
 
 
 def ResNet34():
