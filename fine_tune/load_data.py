b101c32203c4f8ef458157c0866284fb8584afae
rmin2000 <135095340+rmin2000@users.noreply.github.com>
2024-10-24 23:23:28 +0800

    Update load_data.py

--- fine_tune/load_data.py
+++ fine_tune/load_data.py
@@ -4,22 +4,14 @@ from PIL import Image
 import numpy as np
 
 class CustomDataset(Dataset):
-    def __init__(self, img_list, label_list, transform=None):
+    def __init__(self, img_list):
         self.img_list = img_list
-        self.label_list = label_list
-        self.transform = transform
         
     def __len__(self):
         return len(self.img_list)
     
     def __getitem__(self, idx):
-        img = self.img_list[idx]
-        label = self.label_list[idx]
-        label = np.int64(label)
-        if self.transform:
-            img = self.transform(img)
-        
-        return img, label
+        return self.img_list[idx][0], self.img_list[idx][1]
 
 
 class CustomDataset_v2(Dataset):
@@ -27,7 +19,6 @@ class CustomDataset_v2(Dataset):
         self.img_list = img_list
         self.transform = transform
         self.attack_target = int(attack_target)
-        
         
     def __len__(self):
         return len(self.img_list)
@@ -37,4 +28,7 @@ class CustomDataset_v2(Dataset):
         if self.transform:
             img = self.transform(img)
         return img, self.attack_target
+
+
+
 
