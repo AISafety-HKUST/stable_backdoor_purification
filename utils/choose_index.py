e1641a5c99f9ac3e0277c7c5f3e58ffef1b65432
rmin2000 <135095340+rmin2000@users.noreply.github.com>
2024-10-25 00:09:21 +0800

    Update choose_index.py

--- utils/choose_index.py
+++ utils/choose_index.py
@@ -1,6 +1,7 @@
 import random
 import sys, argparse, yaml
 import numpy as np
+from typing import  List
 
 sys.path.append('../')
 
@@ -10,4 +11,22 @@ def choose_index(args, data_all_length) :
         ran_idx = random.sample(range(data_all_length),int(data_all_length*args.ratio))
     else:
         ran_idx = np.loadtxt(args.index, dtype=int)
+    return ran_idx
+
+def choose_by_class(args,bn_train_dataset):
+    by_class: List[List[int]] = [[] for _ in range(args.num_classes)]
+    length = len(bn_train_dataset)
+    for img, label, original_index, _,_ in bn_train_dataset:
+        by_class[label].append(original_index)
+    ran_idx_all = []
+    for class_ in range(args.num_classes):
+        ran_idx = np.random.choice(by_class[class_],int(length*args.ratio/args.num_classes),replace=False)
+        ran_idx_all += ran_idx.tolist()
+    return ran_idx_all
+
+
+def choose_index_v2(ratio, data_all_length) :
+    # choose clean data according to index
+    ran_idx = random.sample(range(data_all_length),int(data_all_length*ratio))
+   
     return ran_idx
